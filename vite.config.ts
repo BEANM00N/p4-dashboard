c030bfde7d090ab226aa3e063ecdec3ba127de7b
BEANM00N <joshmccamley@gmail.com>
2026-08-09 23:48:57 +0100

    config file update

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
 import { createAppConfig } from '@nextcloud/vite-config'
-import { join, resolve } from 'path'
 
 export default createAppConfig(
     {
-        main: resolve(join('src', 'main.js')),
+        main: 'src/main.js',
     },
     {
         createEmptyCSSEntryPoints: true,
-        extractLicenseInformation: true,
-        thirdPartyLicense: false,
-    },
-    {
-        build: {
-            cssCodeSplit: false, // Prevents splitting CSS into separate .chunk.css files
-        },
-        server: {
-            watch: {
-                usePolling: true,
+        config: {
+            build: {
+                cssCodeSplit: false, // Bundles all 21 kB of CSS into perforcedashboard-main.css
+            },
+            server: {
+                watch: {
+                    usePolling: true,
+                },
             },
         },
     },
